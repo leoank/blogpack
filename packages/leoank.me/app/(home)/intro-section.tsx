1f920d99f7fdf68a497f9c6f436beb895f2ed72e
Ankit Kumar <ankits.kvmdam@hotmail.com>
2025-09-27 23:47:20 +0900

    feat: update landing page

--- packages/leoank.me/app/(home)/intro-section.tsx
+++ packages/leoank.me/app/(home)/intro-section.tsx
@@ -1,29 +1,44 @@
 import { Anchor } from "@/components/anchor";
-import { introSectionData } from "@/data/intro-section";
 
 export function IntroSection() {
   return (
-    <section className="relative pt-16">
-      <h1 className="font-light text-4xl mb-4 md:text-7xl">
-        {introSectionData.name}
-      </h1>
+    <section className="relative pt-4">
       <div className="mb-4">
-        <p>{introSectionData.position}</p>
+        <p>Research associate, Broad Institute of MIT and Harvard</p>
         <ul>
-          {introSectionData.contact.map((contact) => (
-            <li key={contact.link}>
-              <Anchor href={contact.link} target="_blank">
-                {contact.text}
-              </Anchor>
-            </li>
-          ))}
+          <li>
+            <Anchor href="mailto:leoank98@gmail.com" target="_blank">
+              leoank98@gmail.com
+            </Anchor>
+          </li>
         </ul>
       </div>
       <div className="flex justify-end">
-        <div
-          className="flex-1 space-y-5"
-          dangerouslySetInnerHTML={{ __html: introSectionData.aboutMe }}
-        />
+        <div className="flex-1 space-y-5">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+          </p>
+        </div>
       </div>
     </section>
   );
--- packages/leoank.me/app/(home)/navbar.tsx
+++ packages/leoank.me/app/(home)/navbar.tsx
@@ -2,15 +2,18 @@ import Image from "next/image";
 
 export function Navbar() {
   return (
-    <nav className="relative flex items-end h-32">
-      <div>
+    <nav className="relative flex items-end h-32 select-none">
+      <div className="flex gap-3 items-center">
         <Image
-          className="h-16 w-16"
+          className="size-8 mt-1"
           height={64}
           width={64}
           src="/assets/logo.svg"
           alt="A"
         />
+        <div>
+          <h1 className="font-light text-3xl">Ankur Kumar</h1>
+        </div>
       </div>
     </nav>
   );
