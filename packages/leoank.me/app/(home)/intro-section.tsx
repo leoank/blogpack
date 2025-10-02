5bfb6e3a53deccafe1f3443913b8493c3cdef569
Ankur Kumar <ank@leoank.me>
2025-10-02 06:09:33 -0400

    chore(content): add wip

--- packages/leoank.me/app/(home)/intro-section.tsx
+++ packages/leoank.me/app/(home)/intro-section.tsx
@@ -4,16 +4,7 @@ export function IntroSection() {
   return (
     <section className="py-4 mb-8">
       <p>
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using &apos;Content here, content here&apos;,
-        making it look like readable English. Many desktop publishing packages
-        and web page editors now use Lorem Ipsum as their default model text,
-        and a search for &apos;lorem ipsum&apos; will uncover many web sites
-        still in their infancy. Various versions have evolved over the years,
-        sometimes by accident, sometimes on purpose (injected humour and the
-        like).
+        Work in progress!
       </p>
     </section>
   );
