6902c378b3d26584b49800db7d4a28e2b652b87c
Ankit Kumar <ankits.kvmdam@hotmail.com>
2025-10-13 21:45:51 +0900

    chore(code block): fix copy button position when overflow.

--- packages/leoank.me/app/(non-home)/blog/[slug]/code-renderer.tsx
+++ packages/leoank.me/app/(non-home)/blog/[slug]/code-renderer.tsx
@@ -49,57 +49,59 @@ export function CodeRenderer(props: TCodeRendererProps) {
   }, [children]);
 
   return (
-    <CodeBlock
-      data-has-linenumber
-      code={children}
-      language={language}
-      lines={lines}
-      words={words}
-    >
-      <CodeBlock.Code className={clsx(showLineNumbers && "has-linenumber")}>
-        {({ isLineHighlighted }) => (
-          <div
-            className={clsx("table-row", isLineHighlighted && "highlighted")}
-          >
-            {showLineNumbers && (
-              <CodeBlock.LineNumber
-                className={
-                  "table-cell pr-4 pl-6 text-sm text-right select-none linenumber"
-                }
-              />
-            )}
-
-            <CodeBlock.LineContent className="table-cell w-full pr-6">
-              <CodeBlock.Token>
-                {({ isTokenHighlighted, children }) => (
-                  <span
-                    className={clsx(
-                      isTokenHighlighted &&
-                        "bg-highlight text-highlight-foreground rounded-md px-1 py-0.5"
-                    )}
-                  >
-                    {children}
-                  </span>
-                )}
-              </CodeBlock.Token>
-            </CodeBlock.LineContent>
-            <button
-              className="bg-background/30 text-foreground rounded-full px-3.5 py-1.5 absolute top-2 right-2 text-sm font-semibold"
-              onClick={copyCode}
+    <div className="relative">
+      <CodeBlock
+        data-has-linenumber
+        code={children}
+        language={language}
+        lines={lines}
+        words={words}
+      >
+        <CodeBlock.Code className={clsx(showLineNumbers && "has-linenumber")}>
+          {({ isLineHighlighted }) => (
+            <div
+              className={clsx("table-row", isLineHighlighted && "highlighted")}
             >
-              {state.value && hasCopied ? (
-                <span className="flex items-center">
-                  <ClipboardCheckIcon className="stroke-white size-4" /> Copied!
-                </span>
-              ) : (
-                <span className="flex items-center">
-                  <ClipboardIcon className="stroke-white size-4" /> Copy
-                </span>
+              {showLineNumbers && (
+                <CodeBlock.LineNumber
+                  className={
+                    "table-cell pr-4 pl-6 text-sm text-right select-none linenumber"
+                  }
+                />
               )}
-            </button>
-          </div>
+
+              <CodeBlock.LineContent className="table-cell w-full pr-6">
+                <CodeBlock.Token>
+                  {({ isTokenHighlighted, children }) => (
+                    <span
+                      className={clsx(
+                        isTokenHighlighted &&
+                          "bg-highlight text-highlight-foreground rounded-md px-1 py-0.5"
+                      )}
+                    >
+                      {children}
+                    </span>
+                  )}
+                </CodeBlock.Token>
+              </CodeBlock.LineContent>
+            </div>
+          )}
+        </CodeBlock.Code>
+      </CodeBlock>
+      <button
+        className="bg-background/30 text-foreground rounded-full px-3.5 py-1.5 absolute top-2 right-2 text-sm font-semibold"
+        onClick={copyCode}
+      >
+        {state.value && hasCopied ? (
+          <span className="flex items-center">
+            <ClipboardCheckIcon className="stroke-white size-4" /> Copied!
+          </span>
+        ) : (
+          <span className="flex items-center">
+            <ClipboardIcon className="stroke-white size-4" /> Copy
+          </span>
         )}
-      </CodeBlock.Code>
-    </CodeBlock>
+      </button>
+    </div>
   );
 }
