4e577c387792d1dacf69778a962c32ef4f8dc78e
Ankur Kumar <ank@leoank.me>
2025-10-16 12:56:11 -0400

    content(blog): update main and add starrynight

--- packages/leoank.me/mdx/blogs/all.ts
+++ packages/leoank.me/mdx/blogs/all.ts
@@ -3,4 +3,4 @@
  * Cloudflare worker doesn't support fs. {@link https://developers.cloudflare.com/workers/runtime-apis/nodejs/#nodejs-api-polyfills}
  * So, we are keeping the record of all the blogs here.
  */
-export const allBlogPostSlug = ["welcome", "welcome2"];
+export const allBlogPostSlug = ["starrynight"];
